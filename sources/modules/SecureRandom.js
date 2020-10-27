94c29d5083ab9747e7f5e01e9a6f1f5991526e1d
David Morcillo <david@codegram.com>
2020-10-27 12:15:07 +0100

    refactor: ask for bytes instead of bits

--- sources/modules/SecureRandom.js
+++ sources/modules/SecureRandom.js
@@ -1,9 +1,8 @@
 import { bigInt } from "./BigInteger.js";
 
 // Returns a secure random number below n
-export const randomBelow = (n, bits = 256) => {
-    const slicesCount = Math.ceil(bits / 8);
-    const randomNumbers = Array.from(crypto.getRandomValues(new Uint8Array(slicesCount)));
+export const randomBelow = (n, bytes_number = 32) => {
+    const randomNumbers = Array.from(crypto.getRandomValues(new Uint8Array(bytes_number)));
     const resultStr = randomNumbers.map(elem => elem.toString(16).padStart(2, "0")).join("")
     const result = bigInt(resultStr, 16);
 
