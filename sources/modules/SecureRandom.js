074e6aadad03d4f0f72f6452ef659b6c59b5dd75
David Morcillo <david@codegram.com>
2020-10-27 12:13:21 +0100

    fix: better implementation of randomBelow

--- sources/modules/SecureRandom.js
+++ sources/modules/SecureRandom.js
@@ -1,11 +1,11 @@
 import { bigInt } from "./BigInteger.js";
 
 // Returns a secure random number below n
-export var randomBelow = (n, bits = 256) => {
-    const slicesCount = ~~(bits / 32);
-    const randomNumbers = crypto.getRandomValues(new Uint32Array(slicesCount));
-    const resultStr = randomNumbers.reduce((acc, elem) => `${acc}${elem}`);
-    const result = bigInt(resultStr);
+export const randomBelow = (n, bits = 256) => {
+    const slicesCount = Math.ceil(bits / 8);
+    const randomNumbers = Array.from(crypto.getRandomValues(new Uint8Array(slicesCount)));
+    const resultStr = randomNumbers.map(elem => elem.toString(16).padStart(2, "0")).join("")
+    const result = bigInt(resultStr, 16);
 
     if (result.lesser(n)) {
         return result;
